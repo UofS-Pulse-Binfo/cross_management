ebca2cfbaae2154b6f774aa44e0e2983d9d2741a
Lacey Sanderson <laceysanderson@781094.no-reply.drupal.org>
2015-09-02 09:08:23 -0600

    Updated the register crosses form to link the newly created cross stock to the requested cross and used this information to make the progress bar reflect the current crossing block.

--- theme/js/CrossManage_registerCrossesProgress.js
+++ theme/js/CrossManage_registerCrossesProgress.js
@@ -2,7 +2,7 @@ Drupal.behaviors.crossmanageRegisterCrossesProgress = {
   attach: function (context, settings) {
 
     var fontFamily = '"Helvetica Neue", Helvetica, Arial, sans-serif';
-    
+
     // Define the chart dimensions & margins.
     var margins = {
         top: 50,
@@ -17,16 +17,16 @@ Drupal.behaviors.crossmanageRegisterCrossesProgress = {
     // Define the dataset.
     // The progress bar consists of 2 portions, the correct and
     // incorrect items.
-    var correct = 305;
-    var incorrect = 25;
+    var correct = Drupal.settings.crossManage.correct;
+    var incorrect = Drupal.settings.crossManage.incorrect;
     // The total expected is the number of expected correct items
     // therefore we need to add the incorrect items to get the
     // total length of the bar when complete.
-    var totalExpected = 439;
+    var totalExpected = Drupal.settings.crossManage.expected;
     var total = totalExpected + incorrect;
-
+debugger;
     // Finally, we need to determine the scale for the
-    // progressbar based on the total count and the 
+    // progressbar based on the total count and the
     // size of our canvas.
     // Remember, domain is the min,max value of the input data
     // and range is the min,max number of pixels that data
@@ -34,18 +34,18 @@ Drupal.behaviors.crossmanageRegisterCrossesProgress = {
     var xScale = d3.scale.linear()
       .domain([0, total])
       .range([0, width]);
-      
+
     // Create a function to draw the 'Expected' line since it needs to be done often.
     var drawExpected = function() {
       svg.selectAll('.expected-line').remove();
-      
+
       // If there is not enough room between the end of the graph and the expected line
       // to draw the label on the right side.
       condition1 = xScale(incorrect) < 50;
       // If there is not enough room between the length of the correct bar and the
       // expected line to draw the label on the left side.
       condition2 = (xScale(totalExpected) - xScale(correct)) < 50;
-      
+
       line = svg.append('svg:line')
         .classed('expected-line', true)
         .attr('x1', xScale(totalExpected))
@@ -53,7 +53,7 @@ Drupal.behaviors.crossmanageRegisterCrossesProgress = {
         .attr('x2', xScale(totalExpected))
         .attr('y2', height)
         .style('stroke', 'black');
-        
+
       label = svg.append('text')
         .classed('expected-line', true)
         .attr('x', xScale(totalExpected)+2)
@@ -62,14 +62,14 @@ Drupal.behaviors.crossmanageRegisterCrossesProgress = {
         .attr('font-family', fontFamily)
         .attr('font-size', '10px')
         .attr('text-anchor', 'start');
-        
+
         // If condition1 (see top of function) then draw the label on the right side
         // of the expected line.
         if (condition1) {
           label.attr('x', xScale(totalExpected)-2)
             .attr('text-anchor','end');
         }
-        
+
         // If condition1 & 2 (see top of function) then move the label above the
         // 'Requested' label while keeping it on the left side.
         if (condition1 && condition2) {
@@ -97,7 +97,7 @@ Drupal.behaviors.crossmanageRegisterCrossesProgress = {
       .attr('font-style', 'italic')
       .attr('font-size', '16px')
       .attr('fill', 'black');
-      
+
     // First we draw the empty bar.
     svg.append('rect')
       .classed('empty', true)
@@ -108,7 +108,7 @@ Drupal.behaviors.crossmanageRegisterCrossesProgress = {
 
     // Now indicate where we expect the correct bar to reach.
     drawExpected();
-  
+
     // Now we finally get to draw our bars :).
     // First we draw the correct items bar.
     svg.append('rect')
@@ -138,7 +138,7 @@ Drupal.behaviors.crossmanageRegisterCrossesProgress = {
         .duration(900)
         .delay(100);
     }
-    
+
     // Correct Number of crosses Label.
     if (xScale(correct) > 40) {
       numLabel = svg.append('text')
@@ -157,10 +157,10 @@ Drupal.behaviors.crossmanageRegisterCrossesProgress = {
         .duration(920)
         .delay(80);
     }
-    
+
     // Redraw the expected line so we can see it.
     drawExpected();
-      
+
     // Now we draw the incorrect items bar.
     // Notice that we set the x to the width of the correct items bar.
     if (incorrect) {
@@ -189,10 +189,10 @@ Drupal.behaviors.crossmanageRegisterCrossesProgress = {
         .transition()
           .attr('x', xScale(correct+incorrect)-2)
           .duration(1000);
-        
+
         // Redraw the expected line so we can see it.
         drawExpected();
-        
+
         // Correct Number of crosses Label.
         if (xScale(incorrect) > 40) {
           numLabel = svg.append('text')
